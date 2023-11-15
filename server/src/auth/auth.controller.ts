de9e7fb8dc7207dc920fbd74f6d3d568c8e7a47c
U2D-8Bits <u2d.8bits@gmail.com>
2023-11-15 13:11:47 -0500

    guards update

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto, LoginDto, UpdateUserDto, CreateUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -12,31 +14,45 @@ export class AuthController {
     return this.authService.create(createUserDto);
   }
 
+
+  // Ruta para Login
   @Post('/login')
   login( @Body() loginDto: LoginDto ){
     return this.authService.login(loginDto);
   }
 
+  // Ruta para register
   @Post('/register')
   register( @Body() registerUserDto: RegisterUserDto ){
     return this.authService.register(registerUserDto);
   }
 
-  @Get()
-  findAll() {
+  // Ruta para obtener todos los usuarios de la App
+  @UseGuards( AuthGuard )
+  @Get('/getUsers')
+  findAll( @Request() req:Request ) {
     return this.authService.findAll();
   }
 
-  @Get(':id')
+  //Ruta para obtener un solo usuario especifico por ID
+  @Get('/user:id')
   findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+    return this.authService.findUserByID(id);
   }
 
+  //Modificar un usuario por ID
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.authService.update(+id, updateUserDto);
   }
 
+  //Modificar a isActive = false por ID
+  @Patch(':id')
+  stateUser( @Param('id') id:string, @Body() UpdateUserDto: UpdateUserDto){
+    return this.authService.update(+id, UpdateUserDto);
+  }
+
+  //Borrar un usuario por ID
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
