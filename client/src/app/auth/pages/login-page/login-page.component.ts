6bdb8bd7459510500e0b9aad1eb85f815b012e5e
U2D-8Bits <u2d.8bits@gmail.com>
2023-11-21 12:15:46 -0500

    General Update

--- client/src/app/auth/pages/login-page/login-page.component.ts
+++ client/src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
+import Swal from 'sweetalert2'
+
 @Component({
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
@@ -24,8 +26,45 @@ export class LoginPageComponent {
     const { username, password } = this.myForm.value;
 
     this.authService.login(username, password)
-      .subscribe( success =>{
-        console.log({success});
+      .subscribe({
+        next: () => {
+          Swal.fire({
+            title: 'Bienvenido!',
+            text: 'Iniciando sesion',
+            icon: 'success',
+            confirmButtonColor: '#3B82F6',
+            confirmButtonText: 'Ok'
+          })
+        },
+        error: (errorMessage) => {
+          if(errorMessage === 'Internal server error' ){
+            Swal.fire({
+              title: 'Usuario Incorrecto',
+              text: 'Nombre de usuario incorrecto',
+              icon: 'error',
+              confirmButtonColor: '#3B82F6',
+              confirmButtonText: 'Ok'
+            })
+          }
+          if(errorMessage === 'Wrong password'){
+            Swal.fire({
+              title: 'Contraseña Incorrecta',
+              text: 'Asegurese que su contraseña sea la correcta',
+              icon: 'error',
+              confirmButtonColor: '#3B82F6',
+              confirmButtonText: 'Ok'
+            })
+          }
+          if(errorMessage === 'User is not active'){
+            Swal.fire({
+              title: 'Usuario Bloqueado',
+              text: 'Su usuario se encuentra bloqueado, solicite desbloqueo',
+              icon: 'warning',
+              confirmButtonColor: '#3B82F6',
+              confirmButtonText: 'Ok'
+            })
+          }
+        }
       });
   }
 
