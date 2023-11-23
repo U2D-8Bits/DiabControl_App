183c65a98e4e9fa6807a5ed98b4c7fd6bf16c13e
U2D-8Bits <u2d.8bits@gmail.com>
2023-11-23 11:13:01 -0500

    Server Configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV.trim()}.env`,
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
