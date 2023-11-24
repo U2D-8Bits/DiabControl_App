984b15621f8f160836f72aa95edef9642537195f
U2D8Bits <u2d.8bits@gmail.com>
2023-11-24 10:01:25 -0500

    new backend

---
+++ server/src/config/variables.config.js
@@ -0,0 +1,7 @@
+export const dbVariables={
+    dbUser: process.env.DB_USER,
+    dbServer: process.env.DB_SERVER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbDialect: process.env.DB_DIALECT,
+    dbName:process.env.DB_NAME
+}
