fedb8d4a60a080a41e603944de63a438372d795e
U2D8Bits <u2d.8bits@gmail.com>
2023-11-25 16:13:31 -0500

    Env Update

--- client/src/environments/environments.ts
+++ client/src/environments/environments.ts
@@ -1,12 +1,4 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:3000',
-    // firebaseConfig: {
-    //     apiKey: "AIzaSyC-3Zu4Kw9yS2s6U8kWVJ6ZQ2wXJ3Qf1f4",
-    //     authDomain: "angular-adv-1a2b3.firebaseapp.com",
-    //     projectId: "angular-adv-1a2b3",
-    //     storageBucket: "angular-adv-1a2b3.appspot.com",
-    //     messagingSenderId: "1093693223040",
-    //     appId: "1:1093693223040:web:6d6d9d9e2e3b6c6b9b0b0b"
-    // }
+    baseUrl: 'http://localhost:3603',
 }
