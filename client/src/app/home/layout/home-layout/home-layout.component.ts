4e9245b8070c34c4c2e3f2583aa5bf24a47ebb70
U2D8Bits <u2d.8bits@gmail.com>
2024-04-20 15:05:25 -0500

    HomeComponent Update

--- client/src/app/home/layout/home-layout/home-layout.component.ts
+++ client/src/app/home/layout/home-layout/home-layout.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'home-home-layout',
   templateUrl: './home-layout.component.html',
-  styleUrl: './home-layout.component.css'
+  styleUrl: './home-layout.component.css',
 })
-export class HomeLayoutComponent {
+export class HomeLayoutComponent implements OnInit {
 
+  userItems: MenuItem[] | undefined;
+
+  activeItem: MenuItem | undefined;
+
+  ngOnInit() {
+
+    this.userItems = [
+      { label: 'Inicio', icon: 'pi pi-fw pi-home', routerLink: '/home/' },
+      { label: 'Formulario', icon: 'pi pi-fw pi-clipboard', routerLink: '/home/forms',},
+      { label: 'Informes',icon: 'pi pi-fw pi-book',routerLink: '/home/informs',},
+      { label: 'Chats', icon: 'pi pi-fw pi-comments', routerLink: '/home/chats' },
+      { label: 'Acerca',icon: 'pi pi-fw pi-file',routerLink: '/home/education',},
+      {
+        icon: 'pi pi-cog',
+        items:[
+          {label: "Mi Cuenta", icon:'pi pi-id-card', routerLink: '/home/settings'},
+          {label: "Salir", icon:'pi pi-sign-out', routerLink:'/auth/'}
+        ]
+      }
+    ]
+  }
 }
