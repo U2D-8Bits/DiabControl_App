fe8b3b0c2a12371faf0b1bf19c9a620f2a80c20d
U2D8Bits <u2d.8bits@gmail.com>
2024-04-24 23:34:48 -0500

    PatientViewPage Update

--- client/src/app/home/pages/patient-view-page/patient-view-page.component.ts
+++ client/src/app/home/pages/patient-view-page/patient-view-page.component.ts
@@ -12,6 +12,8 @@ import { switchMap } from 'rxjs';
 export class PatientViewPageComponent implements OnInit {
 
   public patient?: User;
+  private id?: string;
+  public editInfo: boolean = false;
 
   constructor(
     private userService: AuthUserService,
@@ -33,8 +35,56 @@ export class PatientViewPageComponent implements OnInit {
     });
  }
 
- ngOnDestroy(): void {
-  console.log("Destroying PatientViewPageComponent");
- }
+  // Metodo para regresar a /home/pacientes
+    goBack(){
+      this.router.navigateByUrl('/home/pacientes');
+    }
+
+    //Metodo para editar el paciente
+    editPatient(){
+      // Quitamos el [disabled]="true" a todos los p-button
+      const nameUser = document.getElementById('nameUser');
+      if (nameUser) nameUser.removeAttribute('disabled');
+      if (nameUser) nameUser.setAttribute('required', 'true');
+
+      const lastanameUser = document.getElementById('lastnameUser');
+      if (lastanameUser) lastanameUser.removeAttribute('disabled');
+      if (lastanameUser) lastanameUser.setAttribute('required', 'true');
+
+      const phoneUser = document.getElementById('phoneUser');
+      if (phoneUser) phoneUser.removeAttribute('disabled');
+      if (phoneUser) phoneUser.setAttribute('required', 'true');
+
+      const emailUser = document.getElementById('emailUser');
+      if (emailUser) emailUser.removeAttribute('disabled');
+      if (emailUser) emailUser.setAttribute('required', 'true');
+
+      const passwordUser = document.getElementById('passwordUser');
+      if (passwordUser) passwordUser.removeAttribute('disabled');
+      if (passwordUser) passwordUser.setAttribute('required', 'true');
+
+      const usernameUser = document.getElementById('usernameUser');
+      if (usernameUser) usernameUser.removeAttribute('disabled');
+      if (usernameUser) usernameUser.setAttribute('required', 'true');
+
+      // Modificamos el editInfo a True
+      this.boolInfo();
+    }
+
+    // Metodo para cambiar el editInfo
+    boolInfo(): void {
+      this.editInfo = true;
+      const editButton = document.querySelector('#editButton');
+      if (editButton) editButton.setAttribute('disabled', 'true');
+    }
+
+    // Metodo para cancelar la edit
+    cancelEdit(){
+      this.router.navigate(['/home/patients', this.patient?.id]);
+    }
+
+   ngOnDestroy(): void {
+    console.log("Destroying PatientViewPageComponent");
+   }
 
 }
