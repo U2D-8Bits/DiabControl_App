afa0dbcb5a0a70a74c3c0e3b6fe8f6047124ad17
U2D8Bits <u2d.8bits@gmail.com>
2024-04-25 00:00:03 -0500

    ConfirmDialog Implements

--- client/src/app/home/pages/patient-view-page/patient-view-page.component.ts
+++ client/src/app/home/pages/patient-view-page/patient-view-page.component.ts
@@ -4,13 +4,16 @@ import { User } from '../../../auth/interfaces/user.interface';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
 
+import Swal from 'sweetalert2'
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 @Component({
   selector: 'app-patient-view-page',
   templateUrl: './patient-view-page.component.html',
-  styleUrl: './patient-view-page.component.css'
+  styleUrl: './patient-view-page.component.css',
+  providers: [ConfirmationService, MessageService],
 })
 export class PatientViewPageComponent implements OnInit {
-
   public patient?: User;
   private id?: string;
   public editInfo: boolean = false;
@@ -18,73 +21,96 @@ export class PatientViewPageComponent implements OnInit {
   constructor(
     private userService: AuthUserService,
     private activeRoute: ActivatedRoute,
-    private router: Router
-  ){}
+    private router: Router,
 
- ngOnInit(): void {
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) {}
 
+  ngOnInit(): void {
     this.activeRoute.params
-    .pipe(
-      switchMap(({ id }) => this.userService.getUserById(id))
-    ).subscribe( user => {
-
-      if(!user) return this.router.navigate(['/home/patients']);
-      this.patient = user;
-      console.log("User Data => ", user);
-      return;
-    });
- }
+      .pipe(switchMap(({ id }) => this.userService.getUserById(id)))
+      .subscribe((user) => {
+        if (!user) return this.router.navigate(['/home/patients']);
+        this.patient = user;
+        console.log('User Data => ', user);
+        return;
+      });
+  }
 
   // Metodo para regresar a /home/pacientes
-    goBack(){
-      this.router.navigateByUrl('/home/pacientes');
-    }
+  goBack() {
+    this.router.navigateByUrl('/home/pacientes');
+  }
 
-    //Metodo para editar el paciente
-    editPatient(){
-      // Quitamos el [disabled]="true" a todos los p-button
-      const nameUser = document.getElementById('nameUser');
-      if (nameUser) nameUser.removeAttribute('disabled');
-      if (nameUser) nameUser.setAttribute('required', 'true');
+  //Metodo para editar el paciente
+  editPatient() {
+    // Quitamos el [disabled]="true" a todos los p-button
+    const nameUser = document.getElementById('nameUser');
+    if (nameUser) nameUser.removeAttribute('disabled');
+    if (nameUser) nameUser.setAttribute('required', 'true');
 
-      const lastanameUser = document.getElementById('lastnameUser');
-      if (lastanameUser) lastanameUser.removeAttribute('disabled');
-      if (lastanameUser) lastanameUser.setAttribute('required', 'true');
+    const lastanameUser = document.getElementById('lastnameUser');
+    if (lastanameUser) lastanameUser.removeAttribute('disabled');
+    if (lastanameUser) lastanameUser.setAttribute('required', 'true');
 
-      const phoneUser = document.getElementById('phoneUser');
-      if (phoneUser) phoneUser.removeAttribute('disabled');
-      if (phoneUser) phoneUser.setAttribute('required', 'true');
+    const phoneUser = document.getElementById('phoneUser');
+    if (phoneUser) phoneUser.removeAttribute('disabled');
+    if (phoneUser) phoneUser.setAttribute('required', 'true');
 
-      const emailUser = document.getElementById('emailUser');
-      if (emailUser) emailUser.removeAttribute('disabled');
-      if (emailUser) emailUser.setAttribute('required', 'true');
+    const emailUser = document.getElementById('emailUser');
+    if (emailUser) emailUser.removeAttribute('disabled');
+    if (emailUser) emailUser.setAttribute('required', 'true');
 
-      const passwordUser = document.getElementById('passwordUser');
-      if (passwordUser) passwordUser.removeAttribute('disabled');
-      if (passwordUser) passwordUser.setAttribute('required', 'true');
+    const passwordUser = document.getElementById('passwordUser');
+    if (passwordUser) passwordUser.removeAttribute('disabled');
+    if (passwordUser) passwordUser.setAttribute('required', 'true');
 
-      const usernameUser = document.getElementById('usernameUser');
-      if (usernameUser) usernameUser.removeAttribute('disabled');
-      if (usernameUser) usernameUser.setAttribute('required', 'true');
+    const usernameUser = document.getElementById('usernameUser');
+    if (usernameUser) usernameUser.removeAttribute('disabled');
+    if (usernameUser) usernameUser.setAttribute('required', 'true');
 
-      // Modificamos el editInfo a True
-      this.boolInfo();
-    }
+    // Modificamos el editInfo a True
+    this.boolInfo();
+  }
 
-    // Metodo para cambiar el editInfo
-    boolInfo(): void {
-      this.editInfo = true;
-      const editButton = document.querySelector('#editButton');
-      if (editButton) editButton.setAttribute('disabled', 'true');
-    }
+  // Metodo para cambiar el editInfo
+  boolInfo(): void {
+    this.editInfo = true;
+    const editButton = document.querySelector('#editButton');
+    if (editButton) editButton.setAttribute('disabled', 'true');
+  }
 
-    // Metodo para cancelar la edit
-    cancelEdit(){
-      this.router.navigate(['/home/patients', this.patient?.id]);
-    }
+  // Metodo para cancelar la edit
+  cancelEdit(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Desea cancelar la operación actual?',
+      header: 'Cancelar Edición',
+      icon: 'pi pi-info-circle',
+      rejectButtonStyleClass: 'p-button-secondary',
+      acceptButtonStyleClass: 'p-button-secondary',
+      acceptIcon: 'pi pi-check mr-2',
+      rejectIcon: 'pi pi-times mr-2',
 
-   ngOnDestroy(): void {
-    console.log("Destroying PatientViewPageComponent");
-   }
+      accept: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Cancelado',
+          detail: 'Edición Cancelada',
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Rechazado',
+          detail: 'Continuando Edición',
+        });
+      },
+    });
+  }
 
+  ngOnDestroy(): void {
+    console.log('Destroying PatientViewPageComponent');
+  }
 }
