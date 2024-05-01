204b6fb3d36d3b6065aad8b4b7196460c65f023a
U2D8Bits <u2d.8bits@gmail.com>
2024-05-01 10:29:49 -0500

    confirmDialog update

--- client/src/app/home/pages/patients-page/patients-page.component.ts
+++ client/src/app/home/pages/patients-page/patients-page.component.ts
@@ -7,15 +7,15 @@ import { UserDataService } from '../../services/user-data.service';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { CreatePatientComponent } from '../../components/create-patient/create-patient.component';
 import { ViewPatientComponent } from '../../components/view-patient/view-patient.component';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-patients-page',
   templateUrl: './patients-page.component.html',
   styleUrls: ['./patients-page.component.css'],
-  providers: [DialogService],
+  providers: [DialogService, ConfirmationService, MessageService],
 })
 export class PatientsPageComponent implements OnInit {
-
   patients: User[] = [];
 
   visible: boolean = false;
@@ -25,71 +25,122 @@ export class PatientsPageComponent implements OnInit {
   private dialogComponentSubscription?: Subscription;
   ref: DynamicDialogRef | undefined;
 
-
   constructor(
     private userService: AuthUserService,
     private userDataService: UserDataService,
     public dialogService: DialogService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
   ) {}
 
   ngOnInit() {
-
     console.log(`Se inicializado el componente ${this.constructor.name}`);
 
-    this.userService.getPatients()
-      // Usar subscribe y pipe
-      .subscribe((patients: User[]) => {
-        this.patients = patients;
-        console.log('Pacientes =>', patients);
-      });
-
+    this.userService.getPatients().subscribe((patients: User[]) => {
+      this.patients = patients;
+      console.log('Pacientes =>', patients);
+    });
   }
 
-  showDialog(componentName: string, headerText: string){
-    if(componentName === 'create'){
+  // Método para mostrar la ventana modal
+  showDialog(componentName: string, headerText: string) {
+    // Mostrar componente registrar paciente
+    if (componentName === 'create') {
       this.ref = this.dialogService.open(CreatePatientComponent, {
         header: headerText,
         width: '70%',
-        contentStyle: {"max-height": "500px", "overflow": "auto"},
+        contentStyle: { 'max-height': '500px', overflow: 'auto' },
       });
     }
-
-    if(componentName === 'view'){
+    // Mostrar componente ver/editar paciente
+    if (componentName === 'view') {
       this.ref = this.dialogService.open(ViewPatientComponent, {
         header: headerText,
         width: '70%',
-        contentStyle: {"max-height": "500px", "overflow": "auto"},
+        contentStyle: { 'max-height': '500px', overflow: 'auto' },
       });
     }
   }
 
-  sendUserID(id:number){
+  // Método para enviar el id del usuario seleccionado
+  sendUserID(id: number) {
     this.userDataService.changeUserId(id);
   }
 
-  changeStatusUser(id: string) {
-    this.userService.getUserById(id)
-    .subscribe( user => {
-      console.log(`UserData Recive =>`, user);
-      const statusChanged = user?.bool_status_user ? false : true;
+  // Metodo para cambiar el estado del usuario
+  changeStatusUser(id: string, event: Event): void {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Esta seguro de realizar esta acción con el paciente?',
+      header: 'Cambiar estado del paciente',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+      rejectButtonStyleClass: 'p-button-text',
+      accept: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Comfirmado',
+          detail: 'Se ha cambiado el estado',
+        });
 
-      this.userService.changeStatusUser(id, statusChanged)
-        .subscribe( user => {
-          console.log(`Status Changed =>`, user);
-          this.ngOnInit();
-        })
-    })
+        this.userService.getUserById(id).subscribe((user) => {
+          console.log(`UserData Recive =>`, user);
+          const statusChanged = user?.bool_status_user ? false : true;
+
+          this.userService
+            .changeStatusUser(id, statusChanged)
+            .subscribe((user) => {
+              console.log(`Status Changed =>`, user);
+              this.ngOnInit();
+            });
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Denegado',
+          detail: 'Ha rechazado la acción',
+          life: 3000,
+        });
+      },
+    });
   }
 
-  deleteUser(id: string): void {
+  deleteUser(id: string, event: Event): void {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Esta seguro de realizar esta acción con el paciente?',
+      header: 'Eliminar paciente',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+      rejectButtonStyleClass: 'p-button-text',
+      accept: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Comfirmado',
+          detail: 'Se ha eliminado el paciente correctamente',
+        });
 
-    this.userService.deleteUser(id)
-      .subscribe( user => {
-        console.log(`User Deleted =>`, user);
-        this.ngOnInit();
-      });
+        this.userService.getUserById(id).subscribe((user) => {
+          console.log(`UserData Recive =>`, user);
+          this.userService.deleteUser(id).subscribe((user) => {
+            console.log(`User Deleted =>`, user);
+            this.ngOnInit();
+          });
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Denegado',
+          detail: 'No se ha eliminado el paciente',
+          life: 3000,
+        });
+      },
+    });
   }
-
 
   ngOnDestroy(): void {
     this.dialogComponentSubscription?.unsubscribe();
