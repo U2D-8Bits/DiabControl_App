3f8b5f17a64a1cb4d2e11fc2921654ef90d31bf6
U2D8Bits <u2d.8bits@gmail.com>
2024-05-02 02:03:24 -0500

    InformPage Update & ToolTip Added

--- client/src/app/home/pages/inform-page/inform-page.component.ts
+++ client/src/app/home/pages/inform-page/inform-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from '../../services/user-data.service';
+import { AuthUserService } from '../../../auth/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { User } from '../../../auth/interfaces/user.interface';
 
 @Component({
   selector: 'app-inform-page',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: AuthUserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  public patient?: User;
 
   ngOnInit() {
+    console.log(`Se ha inicializado el componente`, this.constructor.name)
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.userService.getUserById(id) )
+    ).subscribe( patientRecived => {
+      if(!patientRecived) return this.router.navigate(['/home/pacientes']);
+
+
+      this.patient = patientRecived;
+      console.log({ patient: this.patient})
+      return ;
+    })
+
+
+  }
+
+  ngOnDestroy(): void {
+    console.log(`Se ha destruido el componente`, this.constructor.name)
   }
 
 }
