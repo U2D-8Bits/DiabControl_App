05b2db335cdfe2a4bbb2152db518890161fbabc1
U2D8Bits <u2d.8bits@gmail.com>
2024-05-06 20:17:41 -0500

    Update Client

--- client/src/app/auth/pages/login-page/login-page.component.ts
+++ client/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthUserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'auth-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit{
+
+  constructor(
+    private authService: AuthUserService,
+    private router: Router,
+  ) {}
+
+  // Metodo para iniciar sesion
+  Onlogin():void{
+    this.authService.login('admin', '123456')
+    .subscribe( userData => {
+        console.log('Usuario logueado', userData)
+        this.router.navigate(['/home']);
+    });
+  }
+
+  ngOnInit(): void {
+    console.log(`Se ha inicializado el componente ${this.constructor.name}`)
+  }
+
+  ngOnDestroy(): void {
+    console.log(`Se ha destruido el componente ${this.constructor.name}`)
+  }
 
 }
